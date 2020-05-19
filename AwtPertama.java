9211a249bf222613d9f425ed518605e859d9a79d
firdayanijufri <61897517+firdayanijufri@users.noreply.github.com>
2020-05-19 05:00:45 -0700

    Add files via upload

---
+++ AwtPertama.java
@@ -0,0 +1,18 @@
+package no1;
+
+/*
+Nama      : firdayani_13020180089
+Kelas     : B2
+hari/tgl  : Minggu, 15 Mei 2020
+ */
+
+import java.awt.*;
+public class AwtPertama{
+	public static void main(String[] args){
+		Frame win=new Frame(" Coba AWT");
+		win.setSize (300,200);
+		win.show();
+	}
+}
+
+
---
+++ MouseMotionListener_1.java
@@ -0,0 +1,13 @@
+package no3;
+
+/*
+Nama      : firdayani_13020180089
+Kelas     : B2
+hari/tgl  : Minggu, 15 Mei 2020
+ */
+
+public class MouseMotionListener_1 {
+    public static void main (String[] args) {
+        new MyFrame();
+    }
+}
